ad163033e48469a27489560ed82625383b5ec837
William Novoa <wjnovoam@ufpso.edu.co>
2021-09-24 10:32:12 -0500

    Inicio del proyecto

---
+++ src/main/java/org/alkemy/disney/PreAceleracionDisneyApplication.java
@@ -0,0 +1,13 @@
+package org.alkemy.disney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreAceleracionDisneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreAceleracionDisneyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/alkemy/disney/PreAceleracionDisneyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alkemy.disney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreAceleracionDisneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
