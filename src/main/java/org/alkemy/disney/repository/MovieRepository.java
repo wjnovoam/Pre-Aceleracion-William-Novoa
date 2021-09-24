e9667a9fce9c5e44598854fbcba85245a67bf8c9
William Novoa <wjnovoam@ufpso.edu.co>
2021-09-24 14:57:12 -0500

    Creando el repository de movies

---
+++ src/main/java/org/alkemy/disney/repository/MovieRepository.java
@@ -0,0 +1,13 @@
+package org.alkemy.disney.repository;
+
+import org.alkemy.disney.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MovieRepository  extends JpaRepository<Movie, Long> {
+    List<Movie> findAll();
+    Optional<Movie> findById(Long id);
+    void deleteById(Long id);
+}
